405088843b04867fd78a73321f1506b3d2353c5b
AnaJulia1959 <109487959+AnaJulia1959@users.noreply.github.com>
2022-07-17 20:04:44 -0300

    Update and rename Exercicio 4 - LISTA 1 to Exercicio 4 - LISTA 1.java

--- Exercicio 4 - LISTA 1.java
+++ Exercicio 4 - LISTA 1.java
@@ -1,5 +1,5 @@
-# Exerc-cio_4
-Escreva um programa para ler o ano de nascimento de uma pessoa e escrever uma mensagem que diga se ela poderá ou não votar este ano (não é necessário considerar o mês em que ela nasceu).
+#Arquivos-Codigos
+//Escreva um programa para ler o ano de nascimento de uma pessoa e escrever uma mensagem que diga se ela poderá ou não votar este ano (não é necessário considerar o mês em que ela nasceu).
 
 package lista1;
 import java.util.Scanner;
